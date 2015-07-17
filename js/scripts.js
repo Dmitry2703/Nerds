eab7c8d4c50cb8ae3c60d91c02fd02b8311eb00b
Dmitry Meshcheryakov <dmitri.meshcheryakov@gmail.com>
2015-07-17 14:07:43 +0600

    Изменен путь к файлу с изображением маркера карты.

--- js/scripts.js
+++ js/scripts.js
@@ -40,10 +40,12 @@ function initialize() {
   };
   map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+  var image = "./img/marker.png";
   var myLatLng = new google.maps.LatLng(59.93887,30.3231);
   var beachMarker = new google.maps.Marker( {
     position: myLatLng,
-    map: map
+    map: map,
+    icon: image
   });
 }
 
